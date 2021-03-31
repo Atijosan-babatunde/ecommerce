378197b11a0625d62af74a7ae4bec295fbc46b01
Atijosan-babatunde <79738090+Atijosan-babatunde@users.noreply.github.com>
2021-03-31 17:29:49 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Switch,Route}  from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 
 import './App.css';
 import Navbar from './components/Navbar.js';
@@ -9,15 +9,91 @@ import Cart from './components/Cart.js';
 import Default from './components/Default.js';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { details: undefined, cart: [] };
+    this.setDetail = this.setDetail.bind(this);
+    this.addToCart = this.addToCart.bind(this);
+    this.modifyCartProduct = this.modifyCartProduct.bind(this);
+  }
+
+  setDetail(current) {
+    this.setState({ details: current });
+  }
+
+  addToCart(current) {
+    this.setState({ cart: [...this.state.cart, { ...current, quantity: 1 }] });
+  }
+
+  removeFromCart(id) {
+    this.setState({ cart: [...this.state.cart].filter(item => item.id !== id) });
+  }
+
+  modifyCartProduct(action, id) {
+    const product = this.state.cart.find(item => item.id === id);
+    if (action === "dec" && product.quantity === 1) {
+      return this.removeFromCart(id);
+    }
+
+    this.setState({
+      cart:
+        this.state.cart.map(item => {
+          if (item.id === id) {
+            if (action === 'inc') {
+              item.quantity = item.quantity + 1;
+            } else {
+              item.quantity = item.quantity - 1;
+            }
+            return item;
+          }
+          return item;
+        })
+    })
+
+ 
+ 
+  }
+
+     
+
   render() {
     return (
       <React.Fragment>
         <Navbar />
 
         <Switch>
-          <Route exact path="/" component={ProductList} />
-          <Route path="/details" component={Details} />
-          <Route path="/cart" component={Cart} />
+          <Route
+            exact
+            path="/"
+            render={(props) => (
+              <ProductList
+                state={this.state}
+                addToCart={this.addToCart}
+                setDetail={this.setDetail}
+                cart={this.state.cart}
+              />
+            )}
+          />
+
+          <Route
+            path="/details"
+            render={(props) => (
+              <Details
+                details={this.state.details}
+                cart={this.state.cart}
+                addToCart={this.addToCart}
+              />
+            )} />
+
+          <Route
+            path="/cart"
+            render={(props) => (
+              <Cart
+                cart={this.state.cart}
+                modifyCartProduct={this.modifyCartProduct}
+              />
+            )} />
           <Route component={Default} />
         </Switch>
       </React.Fragment>
